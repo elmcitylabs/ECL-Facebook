f69acaebf9438723a334d5e255fd292aadb6cc90
Dan Loewenherz <dloewenherz@gmail.com>
2011-07-23 22:50:19 -0700

    change version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name = 'ecl_facebook',
-    version = '1.0',
+    version = '0.1',
     url = 'http://elmcitylabs.com',
     license = 'BSD',
     description = 'Easy Facebook integration for Django.',
