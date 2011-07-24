b7e1f73a53b8bd9ca58cc2a609b87405f6bde29d
Dan Loewenherz <dloewenherz@gmail.com>
2011-07-24 12:59:13 -0700

    remove FACEBOOK_APP_ID

--- ecl_facebook/views.py
+++ ecl_facebook/views.py
@@ -5,13 +5,13 @@ import urllib
 import cgi
 
 FACEBOOK_DIALOG_PARAMS = {
-        "client_id": settings.FACEBOOK_APP_ID,
+        "client_id": settings.FACEBOOK_KEY,
         "redirect_uri": settings.FACEBOOK_REDIRECT_URL,
         "scope": settings.FACEBOOK_SCOPE
     }
 
 FACEBOOK_ACCESS_TOKEN_PARAMS = {
-    "client_id": settings.FACEBOOK_APP_ID,
+    "client_id": settings.FACEBOOK_KEY,
     "client_secret": settings.FACEBOOK_SECRET,
     "redirect_uri": settings.FACEBOOK_REDIRECT_URL,
 }
