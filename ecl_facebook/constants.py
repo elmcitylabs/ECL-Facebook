98e4274e61e915fba4599ebb04261a0ec8b782ab
Dan Loewenherz <dloewenherz@gmail.com>
2011-07-24 23:26:15 -0700

    set default value for FACEBOOK_POST_COMPLETE_URL

--- ecl_facebook/constants.py
+++ ecl_facebook/constants.py
@@ -2,16 +2,20 @@ from django.conf import settings
 import urllib
 
 FACEBOOK_DIALOG_PARAMS = {
-        "client_id": settings.FACEBOOK_KEY,
-        "redirect_uri": settings.FACEBOOK_REDIRECT_URL,
-        "scope": settings.FACEBOOK_SCOPE
+        'client_id': settings.FACEBOOK_KEY,
+        'redirect_uri': settings.FACEBOOK_REDIRECT_URL,
+        'scope': settings.FACEBOOK_SCOPE
     }
 
 FACEBOOK_ACCESS_TOKEN_PARAMS = {
-    "client_id": settings.FACEBOOK_KEY,
-    "client_secret": settings.FACEBOOK_SECRET,
-    "redirect_uri": settings.FACEBOOK_REDIRECT_URL,
+    'client_id': settings.FACEBOOK_KEY,
+    'client_secret': settings.FACEBOOK_SECRET,
+    'redirect_uri': settings.FACEBOOK_REDIRECT_URL,
 }
 
 FACEBOOK_DIALOG_URL = "https://www.facebook.com/dialog/oauth?" + urllib.urlencode(FACEBOOK_DIALOG_PARAMS)
+try:
+    FACEBOOK_POST_COMPLETE_URL = settings.FACEBOOK_POST_COMPLETE_URL
+except:
+    FACEBOOK_POST_COMPLETE_URL = "/"
 
