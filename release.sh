05be0aa177346ac2d0be490fea2c40c695f73a71
Dan Loewenherz <dloewenherz@gmail.com>
2011-08-01 10:55:49 -0700

    fix release script

--- release.sh
+++ release.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+git add .
+git cim "bump version to $1"
 git archive --format=tar master > ecl_facebook-$1.tar
 gzip -f ecl_facebook-$1.tar
 s3cmd put ecl_facebook-$1.tar.gz s3://packages.elmcitylabs.com/ -P
