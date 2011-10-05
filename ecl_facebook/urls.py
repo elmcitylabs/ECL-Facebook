2110941e53ff0bc76860049779cfea1d3cf26544
Dan Loewenherz <dloewenherz@gmail.com>
2011-10-05 13:46:23 -0700

    fix url issue

--- ecl_facebook/urls.py
+++ ecl_facebook/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('ecl_facebook.views',
-    url(r'^begin$', 'oauth_facebook_begin', name='oauth-facebook-begin'),
+    url(r'begin$', 'oauth_facebook_begin', name='oauth-facebook-begin'),
 )
 
