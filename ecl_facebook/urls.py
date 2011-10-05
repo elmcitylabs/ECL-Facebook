addc37d759d95b26d0808f265cda62309dbfce99
Dan Loewenherz <dloewenherz@gmail.com>
2011-10-05 13:27:01 -0700

    remove complete url--clogs namespace and you shouldn't use it anyway

--- ecl_facebook/urls.py
+++ ecl_facebook/urls.py
@@ -2,6 +2,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('ecl_facebook.views',
     url(r'^begin$', 'facebook_oauth_begin', name='facebook-oauth-begin'),
-    url(r'^complete$', 'facebook_oauth_complete', name='facebook-oauth-complete'),
 )
 
