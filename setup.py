5e23e4a93dca4e33b2d56b8ab111d547eae36cb6
Dan Loewenherz <dloewenherz@gmail.com>
2012-02-06 16:59:56 -0800

    bump version to 0.3.14

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name = 'ecl_facebook',
-    version = '0.3.13',
+    version = '0.3.14',
     url = 'http://elmcitylabs.com',
     license = 'BSD',
     description = 'Easy Facebook integration for Django.',
