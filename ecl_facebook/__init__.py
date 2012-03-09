28e69893f5ce6499808ad7ff57180915428af6c4
Dan Loewenherz <dloewenherz@gmail.com>
2012-03-09 14:15:33 -0800

    bump version to 0.4.2

--- ecl_facebook/__init__.py
+++ ecl_facebook/__init__.py
@@ -9,7 +9,7 @@ ECL Facebook
 
 """
 
-__version__ = "0.4.1"
+__version__ = "0.4.2"
 __author__ = "Dan Loewenherz"
 __copyright__ = "Copyright 2012, Elm City Labs, LLC"
 __maintainer__ = "Dan Loewenherz"
