72ba416eab1aee12db6c1abbe86edec9f6277179
Dan Loewenherz <dloewenherz@gmail.com>
2012-04-13 16:52:58 -0700

    clean up __init__.py file

--- ecl_facebook/__init__.py
+++ ecl_facebook/__init__.py
@@ -4,15 +4,23 @@
 ECL Facebook
 ~~~~~~~~~~~
 
-:copyright: (c) 2012 Elm City Labs, LLC
+:copyright: © 2012 Elm City Labs LLC
 :license: Apache 2.0, see LICENSE for more details.
-
 """
 
-__version__ = "0.5.0"
-__author__ = "Dan Loewenherz"
-__copyright__ = "Copyright 2012, Elm City Labs, LLC"
-__maintainer__ = "Dan Loewenherz"
-__email__ = "dan@elmcitylabs.com"
-__license__ = "Apache 2.0"
+from .metadata import (
+    __author__,
+    __copyright__,
+    __email__,
+    __license__,
+    __maintainer__,
+    __version__,
+)
+
+from .decorators import facebook_begin, facebook_callback
+
+__all__ = [
+    '__author__', '__copyright__', '__email__', '__license__',
+    '__maintainer__', '__version__', 'facebook_begin', 'facebook_callback'
+]
 
