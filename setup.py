0c8f528300a68d3231cc867712f376fbaa176a36
Dan Loewenherz <dloewenherz@gmail.com>
2012-04-13 16:53:32 -0700

    use new metadata file for information

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env/python
 
-import ecl_facebook
+from ecl_facebook import metadata
 
 try:
     from setuptools import setup
@@ -9,14 +9,14 @@ except ImportError:
 
 setup(
     name = 'ecl_facebook',
-    version = ecl_facebook.__version__,
+    version = metadata.__version__,
     url = 'http://git.elmcitylabs.com/ecl-facebook',
-    license = ecl_facebook.__license__,
+    license = metadata.__license__,
     description = 'Easy Facebook integration for Django.',
-    author = ecl_facebook.__author__,
-    author_email = ecl_facebook.__email__,
+    author = metadata.__author__,
+    author_email = metadata.__email__,
     packages=['ecl_facebook'],
-    install_requires=["objectifier", "django>=1.3"],
+    install_requires=["objectifier"],
     package_data={'': ['LICENSE']},
 )
 
