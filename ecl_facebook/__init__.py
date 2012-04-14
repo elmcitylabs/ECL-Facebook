e8bea0e9deb77fd64706616c8ab7c26d7e6c2780
Dan Loewenherz <dloewenherz@gmail.com>
2012-04-14 09:30:12 -0700

    move django decorators to separate namespace

--- ecl_facebook/__init__.py
+++ ecl_facebook/__init__.py
@@ -17,13 +17,10 @@ from .metadata import (
     __version__,
 )
 
-from .decorators import facebook_begin, facebook_callback
-
 from .facebook import Facebook
 
 __all__ = [
     '__author__', '__copyright__', '__email__', '__license__',
-    '__maintainer__', '__version__', 'facebook_begin', 'facebook_callback',
-    'Facebook',
+    '__maintainer__', '__version__', 'Facebook',
 ]
 
