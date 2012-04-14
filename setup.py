f36f1ac9ab6c26fc8db7ed0349b2cd802e7c09f6
Dan Loewenherz <dloewenherz@gmail.com>
2012-04-14 10:03:41 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -7,12 +7,16 @@ try:
 except ImportError:
     from distutils.core import setup
 
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 setup(
     name = 'ecl_facebook',
     version = metadata.__version__,
     url = 'http://git.elmcitylabs.com/ecl-facebook',
     license = metadata.__license__,
     description = 'Easy Facebook integration for Django.',
+    long_description=read('README.rst'),
     author = metadata.__author__,
     author_email = metadata.__email__,
     packages=['ecl_facebook'],
