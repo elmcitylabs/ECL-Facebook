83704d3b05b282539a26f81e59f61b3b45b93fd2
Dan Loewenherz <dloewenherz@gmail.com>
2012-04-13 20:34:04 -0700

    add facebook backend

---
+++ ecl_facebook/backends.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+from django.db.models import get_model
+
+app_label, model_name = settings.PRIMARY_USER_MODEL.split('.')
+GenericUser = get_model(app_label, model_name)
+
+class FacebookBackend():
+    def authenticate(self, id):
+        try:
+            return GenericUser.objects.get(id=id)
+        except GenericUser.DoesNotExist:
+            return None
+
+    def get_user(self, id):
+        try:
+            return GenericUser.objects.get(pk=id)
+        except GenericUser.DoesNotExist:
+            return None
+
+
