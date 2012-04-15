08bea0ccd625a3fd5ac4fc22a3cfc094d9ae352e
Dan Loewenherz <dloewenherz@gmail.com>
2012-04-15 16:48:05 -0700

    fix bug in decorators and add flash example

---
+++ examples/flask_example/example_app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from ecl_facebook.flask_decorators import facebook_begin, facebook_callback
+
+app = Flask(__name__)
+
+@facebook_begin(app, '/oauth/facebook/begin')
+def oauth_facebook_begin():
+    pass
+
+@app.route('/oauth/facebook/complete')
+@facebook_callback
+def oauth_facebook_complete(token, error):
+    return token
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000, debug=True)
+
