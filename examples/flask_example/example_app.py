2439574561b0992b13c889d14218c0748e95a41c
Dan Loewenherz <dloewenherz@gmail.com>
2012-04-15 16:58:07 -0700

    update comment in flask app

--- examples/flask_example/example_app.py
+++ examples/flask_example/example_app.py
@@ -8,10 +8,17 @@ def oauth_facebook_begin():
     pass
 
 # Alternate method...
-@app.route('/oauth/facebook/complete')
-@facebook_callback
+@facebook_callback('/oauth/facebook/complete')
 def oauth_facebook_complete(token, error):
     return token
+
+# You can also break apart the route specification and the decorator itself.
+# For example:
+#
+#     @app.route('/oauth/facebook/complete')
+#     @facebook_callback
+#     def oauth_facebook_complete(token, error):
+#         return token
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
